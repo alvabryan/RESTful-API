0d8c4500c84a4a9f4a1987ae727af673b4ea0ab9
Bryan Alvarenga <bryanalvarenga@bryans-mbp.attlocal.net>
2021-07-01 12:03:42 -0400

    project skeleton created

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+const config = require('./config/database');
 
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// db connection
+// mongoose.connect(config.database);
+
+// mongoose.connection.on('connected',()=>{
+//     console.log(`connected to database ${config.database}`);
+// });
+
+// mongoose.connection.on('error',(error)=>{
+//     console.log(`Database error ${error}`);
+// });
+
+// routes
 app.get('/', (req, res) => {
-    console.log('Server Reached');
+    res.send("Hello World!");
 });
 
 
+// app listener
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
